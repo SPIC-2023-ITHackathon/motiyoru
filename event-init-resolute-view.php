9b190982a70557f7e7e3aaa0356fdb4a3c1249ae
nonrita <2220025@ecc.ac.jp>
2024-02-22 10:24:51 +0900

    fix:登録したらmenu画面に行くようにしました

--- event-init-resolute-view.php
+++ event-init-resolute-view.php
@@ -1,7 +1,8 @@
 <?php
-  $date = $_POST["date"];
+  $datetime = $_POST["datetime"];
   $budget = $_POST["budget"];
   $location = $_POST["location"];
+  $address = $_POST["address"];
   $id = preg_replace_callback(
     '/x|y/',
     function($m) {
@@ -13,9 +14,10 @@
   $arr = array(
     [
       "id" => $id,
-      "date" => $date,
+      "datetime" => $datetime,
       "budget" => $budget,
       "location" => $location,
+      "address" => $address,
       "attendList" => array(
           "name" => array_fill(0, 5, ""),
           "isAttend" => array_fill(0, 5, "NG"),
@@ -30,21 +32,6 @@
   $arr = json_encode($arr);
   file_put_contents("db.json" , $arr);
 
-  $json = file_get_contents('./db.json');
-  $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
-  $arr = json_decode($json,true);
+  header("Location: ./menu.php");
+  exit;
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-      <p>日時:<?=$arr[0]["date"] ?></p>
-      <p>一人当たりの予算:<?=$arr[0]["budget"] ?></p>
-      <p>場所:<?=$arr[0]["location"] ?></p>
-</body>
-</html>
